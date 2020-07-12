7762fa9b0b523051dc19094d56e951b104a4e0e7
Ray Jung <jungse12@msu.edu>
2020-07-12 05:17:09 -0400

    postgres DB

--- Ford_Project/settings.py
+++ Ford_Project/settings.py
@@ -81,12 +81,25 @@ WSGI_APPLICATION = 'Ford_Project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'fordProject',
+            'USER': 'ford_admin',
+            'PASSWORD': 'adam242529',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
+
 
 
 # Password validation
