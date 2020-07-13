a37e5b573e26a1b3579958d3183ad1e57b8dffac
Ray Jung <jungse12@msu.edu>
2020-07-12 20:31:29 -0400

    df

--- dashboard/views.py
+++ dashboard/views.py
@@ -642,8 +642,8 @@ def calc(request):
         Elec_CED=float(_elec_CED)
         Batt_GWP=float(_bat_GWP)  
         Batt_CED=float(_bat_CED)
-        touList = TouMatrix.objects.all().first().array_list
-        elecConsump = ElectricConsumption.objects.all().first().array_list
+        touList = TouMatrix.objects.all().first().array_list.strip('][').split(', ')
+        elecConsump = ElectricConsumption.objects.all().first().array_list.strip('][').split(', ')
         #print("ELECT:",Elec_GWP, Elec_CED)
         """==============================================================================================================================="""
         """Empty arrays/matrices created to account for holding the battery capacity and state of charge of battery in each hour"""
