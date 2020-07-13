f43317a0aad208af6bd089df5bc89811a0e741a1
Ray Jung <jungse12@msu.edu>
2020-07-12 22:11:45 -0400

    asdf

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -564,7 +564,7 @@ $(document).ready(function() {
             }
         ];
         console.log(data_values_WD); 
-        document.getElementById('tou-matrix').value = JSON.stringify(touRateList);
+        document.getElementById('tou-matrix').value = touRateList;
 
         //console.log("THIS IS TOU: " + touRateList.length)
         var test = 1;
