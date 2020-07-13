adb0a46cf747f9176b9d96f23571feb0a13d0935
Ray Jung <jungse12@msu.edu>
2020-07-12 21:03:26 -0400

    asdf

--- dashboard/views.py
+++ dashboard/views.py
@@ -642,9 +642,9 @@ def calc(request):
         Elec_CED=float(_elec_CED)
         Batt_GWP=float(_bat_GWP)  
         Batt_CED=float(_bat_CED)
-        touMatrix = TouMatrix.objects.all().first().array_list.strip('][').split(',').split(',')
+        touMatrix = TouMatrix.objects.all().first().array_list.strip('][').split(',')
         elecConsump = ElectricConsumption.objects.all().first().array_list.strip('][').split(',')
-        print("THIS IS FKING SIZE ONE ARRAY: ", len(touMatrix))
+        print("THIS IS FKING SIZE ONE ARRAY: ", touMatrix[:48])
 
             
         elecConsump = list(map(float,elecConsump))
