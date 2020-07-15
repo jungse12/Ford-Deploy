7b2220ee302133de1b657ad89b6ffb5522b14914
Ray Jung <jungse12@msu.edu>
2020-07-14 22:13:36 -0400

    asdf

--- Ford_Project/settings.py
+++ Ford_Project/settings.py
@@ -29,9 +29,9 @@ ALLOWED_HOSTS = ['161.35.8.246', '127.0.0.1', 'localhost']
 
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static/')
 ]
 
 STATICFILES_FINDERS = [
