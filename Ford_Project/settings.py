88cef5b2baa30ec94c10e53c36d47edbc95aa9d5
Ray Jung <jungse12@msu.edu>
2020-07-15 00:57:06 -0400

    asdf

--- Ford_Project/settings.py
+++ Ford_Project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+jez(mz=3fk7pe@gmeryp^iv=!axjv99&&xq^#@!%fukesyu@5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['161.35.8.246', '127.0.0.1', 'localhost']
 
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = ['161.35.8.246', '127.0.0.1', 'localhost']
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static_env')
+    STATIC_ROOT,
+    #os.path.join(BASE_DIR, 'static_env')
 ]
 
 STATICFILES_FINDERS = [
