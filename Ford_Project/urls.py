bce453b8820948ca6e3761608357d30e156cd50b
Ray Jung <jungse12@msu.edu>
2020-07-14 22:42:56 -0400

    asdf

--- Ford_Project/urls.py
+++ Ford_Project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
 
 urlpatterns = [
     path('', include('dashboard.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
