735c2f2c06a6a9c7aeb38e94c616990ed6934b14
Ray Jung <jungse12@msu.edu>
2020-07-14 22:50:31 -0400

    asdf

--- Ford_Project/settings.py
+++ Ford_Project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+jez(mz=3fk7pe@gmeryp^iv=!axjv99&&xq^#@!%fukesyu@5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['161.35.8.246', '127.0.0.1', 'localhost']
 
