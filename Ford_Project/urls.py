d7ac470ae94ec373124a424476941e25366bd2ff
Ray Jung <jungse12@msu.edu>
2020-07-14 22:23:41 -0400

    asdf

--- Ford_Project/urls.py
+++ Ford_Project/urls.py
@@ -20,6 +20,3 @@ urlpatterns = [
     path('', include('dashboard.urls')),
     path('admin/', admin.site.urls)
 ]
-if settings.DEBUG:
-    #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
